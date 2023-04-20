ff0ee6d2120e1b83f0639db94f68ebac6fede73e
Jaedyn Keister <jaedyn.keister@gmail.com>
2023-04-20 16:07:28 -0500

    back from react to have a working site

---
+++ app.js
@@ -0,0 +1,138 @@
+//src switch
+
+function switchPage() {
+    var contentFrame = document.getElementById('content');
+    if (contentFrame.src.endsWith('Portfolio-Website/index.html')) {
+        contentFrame.src = 'Portfolio-Website/index.html';
+    } else {
+        contentFrame.src = 'page1.html';
+    }
+}
+
+//hamburger navbar 
+const hamburger = document.querySelector(".hamburger");
+const navbars = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navbars.classList.toggle("active");
+})
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () =>{
+    hamburger.classList.remove("active");
+    navbars.classList.remove("active");
+}))
+
+// main header animation
+
+const texts = ['Welcome', 'Take a look around']
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type(){
+
+if(count === texts.length){
+    count = 0
+}
+currentText = texts[count];
+letter = currentText.slice(0, ++index)
+
+document.querySelector('.main-description').textContent = letter;
+console.log(letter)
+
+
+if(letter.length === currentText.length){
+    count++;
+    index = 0;
+    console.log(currentText)
+}
+setTimeout(type, 350)
+}());
+
+// slide-show
+const slider = document.querySelector('.slide-container')
+const nextBtn = document.querySelector('.next-btn')
+const prevBtn = document.querySelector('.prev-btn')
+const slides = document.querySelectorAll('.slide')
+const slideIcons = document.querySelectorAll(".slide-icon")
+
+let numberOfSlides  = slides.length;
+let slideNumber = 0
+
+//next-btn
+nextBtn.addEventListener('click', () =>{
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    })
+    slideIcons.forEach((slideIcon) => {
+        slideIcon.classList.remove('active');
+    })
+    slideNumber++;
+
+    if(slideNumber > (numberOfSlides -1)){
+        slideNumber = 0
+    }
+
+    slides[slideNumber].classList.add("active")
+    slideIcons[slideNumber].classList.add("active")
+})
+//prev-btn
+prevBtn.addEventListener('click', () =>{
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    })
+    slideIcons.forEach((slideIcon) => {
+        slideIcon.classList.remove('active');
+    })
+    slideNumber--;
+
+    if(slideNumber < 0){
+        slideNumber = numberOfSlides - 1
+    }
+
+    slides[slideNumber].classList.add("active")
+    slideIcons[slideNumber].classList.add("active")
+})
+//repeat when inacitve;
+var playSlider;
+var repeater = () => {
+    playSlider = setInterval(function(){
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        })
+        slideIcons.forEach((slideIcon) => {
+            slideIcon.classList.remove('active');
+        })
+        slideNumber++;
+    
+        if(slideNumber > (numberOfSlides -1)){
+            slideNumber = 0
+        }
+    
+        slides[slideNumber].classList.add("active")
+        slideIcons[slideNumber].classList.add("active")
+    }, 5000)
+}
+repeater();
+
+//when hovering stop
+slider.addEventListener("mouseover", () => {
+    clearInterval(playSlider)
+})
+//when unhovered continue 
+slider.addEventListener("mouseout", () => {
+    repeater()
+})
+
+//email page/button
+const contactButton = document.querySelector(".button-64");
+const contactPage = document.querySelector(".contact");
+
+contactButton.addEventListener("click", () => {
+    contactButton.classList.toggle("active");
+    contactPage.classList.toggle("active");
+})
+
+
