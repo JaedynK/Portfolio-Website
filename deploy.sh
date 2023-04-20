bf436000b79696d7598011e62e1cd944e8544cfb
Jaedyn Keister <jaedyn.keister@gmail.com>
2023-04-20 17:57:43 -0500

    test

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+
+set -e
+
+npm run build
+
+cd dist
+
+echo> .nojekyll
+
+echo 'http://www.jaedynkeister.com' > CNAME
+
+git init
+git checkout -B main
+git add -add
+git commit -m 'deploy'
+
+cd - 
